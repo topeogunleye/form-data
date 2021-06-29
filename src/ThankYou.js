5cbfeecad7d7398c17c056d19f98f634787557cd
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-06-29 11:33:49 +0100

    Makes height

--- src/ThankYou.js
+++ src/ThankYou.js
@@ -1,8 +1,7 @@
 const ThankYou = () => {
   return (
-    <div className="flex justify-center items-center">
-      <h1 className="text-xl">Thank you for filling out your information!
-      </h1>
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-xl">Thank you for filling out your information!</h1>
     </div>
   );
 };
